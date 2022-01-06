91835047ec63cac26f0bf989b426f82cefbd148d
Maksim Smolnikau <gctdljgkjnm@mail.ru>
2022-01-06 21:36:12 +0200

    Released commands for general Window actions.

--- SLS/SLS/MVVM/ViewModel/MainVM.cs
+++ SLS/SLS/MVVM/ViewModel/MainVM.cs
@@ -10,9 +10,29 @@ namespace SLS.MVVM.ViewModel
 {
     internal class MainVM : ObservableObject
     {
+        #region App Title
+
+        private string _title = "Default title.";
+
+        public string Title { get => _title; set => Set<string>(ref _title, value); }
+
+        #endregion
+
         public ObservableCollection<ResourceModel> ResourceCollection { get; }
 
+        // Views
+
+        #region Current view
+
+        static object _currentView;
+
+        public object CurrentView { get => _currentView; set => Set(ref _currentView, value); }
+
+        #endregion
+
         // Commands
+
+        #region Close app COMMAND
 
         public ICommand CloseApplicationCommand;
 
@@ -20,12 +40,41 @@ namespace SLS.MVVM.ViewModel
 
         private void OnCloseApplicationCommandExecuted(object p) => Application.Current.Shutdown();
 
+        #endregion
+
+        #region Maximaize app COMMAND
+
+        public ICommand MaximizeAppCommand;
+
+        private bool CanMaximizeAppCommandExecute(object p) => true;
+
+        private void OnMaximizeAppCommandExecuted(object p) 
+        {
+            if(Application.Current.MainWindow.WindowState != WindowState.Maximized)
+                Application.Current.MainWindow.WindowState = WindowState.Maximized;
+            else
+                Application.Current.MainWindow.WindowState = WindowState.Normal;
+        }
+
+        #endregion
+
+        #region Minimize app COMMAND
+
+        public ICommand MinimizeAppCommand;
+
+        private bool CanMinimizeAppCommandExecute(object p) => true;
+
+        private void OnMinimizeAppCommandExecuted(object p) => Application.Current.MainWindow.WindowState = WindowState.Minimized;
+
+        #endregion
 
         public MainVM()
         {
-            #region
+            #region Commands
 
             CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
+            MaximizeAppCommand = new LambdaCommand(OnMaximizeAppCommandExecuted, CanMaximizeAppCommandExecute);
+            MinimizeAppCommand = new LambdaCommand(OnMinimizeAppCommandExecuted, CanMinimizeAppCommandExecute);
 
             #endregion
 
