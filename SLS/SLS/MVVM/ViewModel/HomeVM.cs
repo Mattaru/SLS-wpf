0603a78add3922530188e3065acb9ae9b337d396
Maksim Smolnikau <gctdljgkjnm@mail.ru>
2022-01-06 21:59:49 +0200

    Maked the HomePage with the first eteration of layout.

---
+++ SLS/SLS/MVVM/ViewModel/HomeVM.cs
@@ -0,0 +1,27 @@
+﻿using SLS.MVVM.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SLS.MVVM.ViewModel
+{
+    internal class HomeVM
+    {
+        public ObservableCollection<ResourceModel> ResourceModels { get; }
+
+        public HomeVM()
+        {
+            var resources = Enumerable.Range(1, 100).Select(i => new ResourceModel
+            {
+                Name = $"Name {i}",
+                Login = $"Login {i}",
+                Password = $"Password {i}"
+            });
+
+            ResourceModels = new ObservableCollection<ResourceModel>(resources);
+        }
+    }
+}
