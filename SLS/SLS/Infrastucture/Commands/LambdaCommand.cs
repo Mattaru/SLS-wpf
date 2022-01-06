d6d71cb6121e11c85eebfbc909c68f2e19fa03dc
Maksim Smolnikau <gctdljgkjnm@mail.ru>
2022-01-06 20:53:32 +0200

    Layout for MainPage and maked Core objects like ObservableObject, RelayCommand and LambdaCommand

---
+++ SLS/SLS/Infrastucture/Commands/LambdaCommand.cs
@@ -0,0 +1,22 @@
+﻿using SLS.Core;
+using System;
+
+namespace SLS.Infrastucture.Commands
+{
+    internal class LambdaCommand : RelayCommand
+    {
+        private Action<object> _Execute;
+
+        private Func<object, bool> _CanExecute;
+
+        public LambdaCommand(Action<object> Execute, Func<object, bool> CanExecute = null)
+        {
+            _Execute = Execute ?? throw new ArgumentNullException(nameof(Execute));
+            _CanExecute = CanExecute;
+        }
+
+        public override bool CanExecute(object? parameter) => _CanExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object? parameter) => _Execute(parameter);
+    }
+}
--- SLS/SLS/MVVM/ViewModel/MainVM.cs
+++ SLS/SLS/MVVM/ViewModel/MainVM.cs
@@ -1,11 +1,10 @@
 ﻿using SLS.Core;
+using SLS.Infrastucture.Commands;
 using SLS.MVVM.Model;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace SLS.MVVM.ViewModel
 {
@@ -13,8 +12,23 @@ namespace SLS.MVVM.ViewModel
     {
         public ObservableCollection<ResourceModel> ResourceCollection { get; }
 
+        // Commands
+
+        public ICommand CloseApplicationCommand;
+
+        private bool CanCloseApplicationCommandExecute(object p) => true;
+
+        private void OnCloseApplicationCommandExecuted(object p) => Application.Current.Shutdown();
+
+
         public MainVM()
         {
+            #region
+
+            CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
+
+            #endregion
+
             var resources = Enumerable.Range(1, 100).Select(i => new ResourceModel
             {
                 Name = $"Name {i}",
