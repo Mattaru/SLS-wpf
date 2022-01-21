e12318545698f108710dbba208896258c8ab8f29
Maksim Smolnikau <gctdljgkjnm@mail.ru>
2022-01-21 14:37:26 +0200

    Added one more realization of OnPropertyChanged method.

--- SLS/SLS/Core/ObservableObject.cs
+++ SLS/SLS/Core/ObservableObject.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Threading;
 
 namespace SLS.Core
 {
@@ -12,7 +13,24 @@ namespace SLS.Core
     {
         public event PropertyChangedEventHandler? PropertyChanged;
 
-        public virtual void OnPropertyChnged([CallerMemberName] string? name = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        public virtual void OnPropertyChnged([CallerMemberName] string? PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+
+            /*var handlers = propertychanged;
+            if (handlers != null) return;
+
+            var invocation_list = handlers.getinvocationlist();
+
+            var args = new propertychangedeventargs(propertyname);
+            foreach(var action in invocation_list)
+            {
+                if (action.target is dispatcherobject disp_obj)
+                    disp_obj.dispatcher.invoke(action, this, args);
+                else
+                    action.dynamicinvoke(this, args);
+            }*/
+        }
 
         public virtual void Set<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
         {
