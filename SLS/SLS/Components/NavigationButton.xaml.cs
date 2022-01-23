6b8e71fe70b600063b55c400c195088cfb2e635b
Maksim Smolnikau <gctdljgkjnm@mail.ru>
2022-01-23 11:04:36 +0200

    Added the NavigationButton Component. Refactored the main nav menu.

---
+++ SLS/SLS/Components/NavigationButton.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace SLS.Components
+{
+    /// <summary>
+    /// Interaction logic for NavigationButton.xaml
+    /// </summary>
+    public partial class NavigationButton : UserControl
+    {
+        #region Icon
+
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register(
+                nameof(Icon),
+                typeof(string),
+                typeof(NavigationButton),
+                new PropertyMetadata(default(string)));
+
+        public string Icon 
+        {
+            get => (string)GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
+
+        #endregion
+
+        #region Text
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register(
+                nameof(Text),
+                typeof(string),
+                typeof(NavigationButton),
+                new PropertyMetadata(default(string)));
+
+        public string Text
+        {
+            get => (string)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
+        #endregion
+
+        #region IsChecked
+
+        public static readonly DependencyProperty IsCheckedProperty =
+            DependencyProperty.Register(
+                nameof(IsChecked),
+                typeof(bool),
+                typeof(NavigationButton),
+                new PropertyMetadata(default(bool)));
+
+        public bool IsChecked
+        {
+            get => (bool)GetValue(IsCheckedProperty);
+            set => SetValue(IsCheckedProperty, value);
+        }
+
+        #endregion
+
+        #region Command
+
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.Register(
+                nameof(Command),
+                typeof(ICommand),
+                typeof(NavigationButton),
+                new PropertyMetadata(null));
+
+        public ICommand Command
+        {
+            get => (ICommand)GetValue(CommandProperty);
+            set => SetValue(CommandProperty, value);
+        }
+
+        #endregion
+
+        public NavigationButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
