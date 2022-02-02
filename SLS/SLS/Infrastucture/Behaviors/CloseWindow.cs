7ecd5605a7612bc91dd240a74f154919ffe6cf83
Maksim Smolnikau <gctdljgkjnm@mail.ru>
2022-02-02 15:08:11 +0200

    Released behaviors for the general window functionality.

---
+++ SLS/SLS/Infrastucture/Behaviors/CloseWindow.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Xaml.Behaviors;
+
+namespace SLS.Infrastucture.Behaviors
+{
+    internal class CloseWindow : Behavior<Button>
+    {
+        protected override void OnAttached() => AssociatedObject.Click += OnButtonClick;
+        
+        protected override void OnDetaching() => AssociatedObject.Click -= OnButtonClick;
+
+        private void OnButtonClick(object sender, RoutedEventArgs e) => (AssociatedObject.FindVisualRoot() as Window)?.Close();
+    }
+}
