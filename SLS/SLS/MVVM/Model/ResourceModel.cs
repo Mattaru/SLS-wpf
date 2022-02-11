5fe3590360eb6a8ed67519afd5aff30d4bbb66ff
Maksim Smolnikau <gctdljgkjnm@mail.ru>
2022-02-11 11:43:50 +0200

    Setters and getters for the resource model.

--- SLS/SLS/MVVM/Model/ResourceModel.cs
+++ SLS/SLS/MVVM/Model/ResourceModel.cs
@@ -5,17 +5,53 @@ namespace SLS.MVVM.Model
 {
     internal class ResourceModel : IEntity
     {
+        private string _name = "Unknown";
+
+        private string _login = "Unknown";
+
+        private string _password = "Unknown";
+
         public int Id { get; set; }
 
-        public string Name { get; set; } = "Unknown";
+        public string Name 
+        { 
+            get => _name;
+            set
+            {
+                if (_name == value) return;
 
-        public string Login { get; set; } = "Unknown";
+                _name = value;
+                LastModificationDate = DateTime.Now;
+            } 
+        } 
 
-        public string Password { get; set; } = "Unknown";
+        public string Login 
+        { 
+            get => _login;
+            set
+            {
+                if (value == _login) return;    
+
+                _login = value;
+                LastModificationDate = DateTime.Now;
+            } 
+        }
+
+        public string Password 
+        { 
+            get => _password;
+            set
+            {
+                if (value != _password) return;
+
+                _password = value;
+                LastModificationDate = DateTime.Now;
+            } 
+        }
 
         public DateTime CreationDate { get; } = DateTime.Now;
 
-        public DateTime LastModificationDate { get; private set; } = DateTime.Now;
+        public DateTime LastModificationDate { get; private set; }
 
         public ResourceModel() { }
 
