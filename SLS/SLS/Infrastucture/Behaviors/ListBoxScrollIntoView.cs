bd981b73104af1005a04708573ce007be9f913e2
Maksim Smolnikau <gctdljgkjnm@mail.ru>
2022-02-18 14:05:22 +0200

    Expended functionality of theListBoxScrollIntoView behavior.

--- SLS/SLS/Infrastucture/Behaviors/ListBoxScrollIntoView.cs
+++ SLS/SLS/Infrastucture/Behaviors/ListBoxScrollIntoView.cs
@@ -4,7 +4,7 @@ using System.Windows.Controls;
 
 namespace SLS.Infrastucture.Behaviors
 {
-    internal class ListBoxScrollIntoView : Behavior<ListBox>
+    internal class ScrollAfterLoaded : Behavior<ListBox>
     {
         protected override void OnAttached() => AssociatedObject.Loaded += AssociatedObject_Loaded;
         
@@ -12,6 +12,21 @@ namespace SLS.Infrastucture.Behaviors
         protected override void OnDetaching() => AssociatedObject.Loaded -= AssociatedObject_Loaded;
 
         private void AssociatedObject_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (!(sender is ListBox listBox)) throw new TypeAccessException($"{sender} is not ListBox.");
+
+            listBox.SelectedItem = listBox.Items[listBox.Items.Count - 1];
+            listBox.ScrollIntoView(listBox.SelectedItem);
+        }
+    }
+
+    internal class ScrollAfterUpdated : Behavior<ListBox>
+    {
+        protected override void OnAttached() => AssociatedObject.SourceUpdated += AssociatedObject_SourceUpdated;
+        
+        protected override void OnDetaching() => AssociatedObject.SourceUpdated -= AssociatedObject_SourceUpdated;
+
+        private void AssociatedObject_SourceUpdated(object? sender, System.Windows.Data.DataTransferEventArgs e)
         {
             if (!(sender is ListBox listBox)) throw new TypeAccessException($"{sender} is not ListBox.");
 
